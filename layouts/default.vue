0c9512d97d1faf1ae1a377c0b8d64e328d1e8590
Aler Denisov <aler.zampillo@gmail.com>
2018-08-02 19:41:34 +0300

    motserrat font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
     <nuxt/>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,15 @@
-<template>
-  <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card v-for="person in people" :key="person.id" :person="person"></Card>
-    </div>
-  </section>
+<template lang="pug">
+  section
+    h1.header Deploeos
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import Card from "~/components/Card.vue"
-
-@Component({
-  components: {
-    Card
-  }
-})
-export default class extends Vue {
-  @State people
-}
+import { Component, Vue } from "nuxt-property-decorator";
+@Component({})
+export default class extends Vue {}
 </script>
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
-    sans-serif;
-}
-
-.cards {
-  display: flex;
-  flex-wrap: wrap;
+<style>
+body {
+  font-family: "Montserrat", sans-serif;
 }
 </style>
