95036b1259c1fd379adbd5ddf9ccb7c90f2acf31
Aler Denisov <aler.zampillo@gmail.com>
2018-08-15 16:25:18 +0300

    boilerplate /interact page

---
+++ pages/interact.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    div(:class="b()")
+</template>
+
+<style lang="scss">
+.interact-page {
+}
+</style>
+
+
+<script lang="ts">
+import Eos from "~/lib/eos";
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+
+@Component({
+  name: "interact-page"
+})
+export default class extends Vue {
+}
+</script>
