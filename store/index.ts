f7cba8b36d4cac45cbc9fb12047cf95c93e60238
Aler Denisov <aler.zampillo@gmail.com>
2018-08-15 16:24:47 +0300

    start move logic to state storage
    add EosClass instance, network

--- store/index.ts
+++ store/index.ts
@@ -1,18 +1,43 @@
-export const state = () => ({
-  people: []
-})
+import EosClass from "lib/eos";
+import { INetworkModel } from "types";
 
-export const mutations = {
-  setPeople(state, people) {
-    state.people = people
+type Nullable<T> = { [P in keyof T]: T[P] | null };
+
+export interface IStorageState {
+  eos: EosClass;
+  abi: any;
+  wast: Buffer;
+
+  network: INetworkModel;
+  selectedIdentity: string;
+}
+
+export function state(): Nullable<IStorageState> {
+  return {
+    eos: null,
+    abi: null,
+    wast: null,
+    network: null,
+    selectedIdentity: null
+  };
+}
+
+export class mutations {
+  static setNetwork(state: IStorageState, network: INetworkModel) {
+    state.network = network;
+  }
+
+  static setIdentity(state: IStorageState, identity: string) {
+    state.selectedIdentity = identity;
+  }
+
+  static setEos(state: IStorageState, eos: EosClass) {
+    state.eos = eos;
   }
 }
 
-export const actions = {
-  async nuxtServerInit({ commit }, { app }) {
-    const people = await app.$axios.$get(
-      "./random-data.json"
-    )
-    commit("setPeople", people.slice(0, 10))
+export class actions {
+  static async setNetwork(ctx, network: INetworkModel) {
+    console.log(ctx);
   }
 }
