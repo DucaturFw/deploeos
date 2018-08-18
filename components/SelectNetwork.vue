46576c296f2216cdc46b3eb5064e3337caf98046
Aler Denisov <aler.zampillo@gmail.com>
2018-08-18 10:35:47 +0300

    Set network directly from component
    Signed-off-by: Aler Denisov <aler.zampillo@gmail.com>

--- components/SelectNetwork.vue
+++ components/SelectNetwork.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div(:class="b()")
     h3(:class="b('label')") Selected Network:
-    el-select(:class="b('select')" v-model='network')
+    el-select(:class="b('select')" v-model='selectedNetwork' placeholder='Select or add')
       el-option(v-for="net in networks" :key="net.id" :label="net.name" :value="net.id")
 </template>
 
@@ -9,13 +9,26 @@
 import { INetworkModel } from "~/types";
 import { NETWORKS } from "~/constants";
 import { Component, Vue } from "nuxt-property-decorator";
+import { Action, State } from "vuex-class";
 
 @Component({
   name: "el-select-network"
 })
 export default class SelectNetwork extends Vue {
+  @State network: INetworkModel;
+  @Action setNetwork;
   networks: INetworkModel[] = NETWORKS;
-  network: string = "0";
+  networkId: string = "";
+
+  get selectedNetwork() {
+    return this.network ? this.network.id : "";
+  }
+
+  set selectedNetwork(id) {
+    const network = this.networks[parseInt(id)];
+    this.setNetwork(network);
+    this.$emit("selected", network);
+  }
 }
 </script>
 
