4e20d6dfb7cc31702168cdce0971c4b40a9cd9c7
Aler Denisov <aler.zampillo@gmail.com>
2018-08-18 10:36:15 +0300

    add chain ids to predefined networks
    Signed-off-by: Aler Denisov <aler.zampillo@gmail.com>

--- constants.ts
+++ constants.ts
@@ -6,7 +6,8 @@ export const NETWORKS: INetworkModel[] = [
     host: "nodes.get-scatter.com",
     port: 443,
     protocol: "https",
-    blockchain: "eos"
+    blockchain: "eos",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
   },
   {
     id: "1",
@@ -14,6 +15,7 @@ export const NETWORKS: INetworkModel[] = [
     host: "jungle.eos.smartz.io",
     port: 443,
     protocol: "https",
-    blockchain: "eos"
+    blockchain: "eos",
+    chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca"
   }
 ];
