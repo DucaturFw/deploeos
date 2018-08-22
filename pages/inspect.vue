9cdd6450d3fd00c5fed69b0fd9fec31fcdbd74f7
Aler Denisov <aler.zampillo@gmail.com>
2018-08-22 18:28:31 +0300

    new inspect identity page
    Signed-off-by: Aler Denisov <aler.zampillo@gmail.com>

--- pages/inspect.vue
+++ pages/inspect.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     div(:class="b()" v-loading='!eos')
-      pre {{ eos }}
+      div 
       //- el-form(:model='contract' label-width="120px")
       //-   el-form-item(label='Contract account')
       //-     el-input(v-model='contract.account')
@@ -16,15 +16,30 @@
 import { Component, Vue } from "nuxt-property-decorator";
 import { State } from "vuex-class";
 import Eos, { EosInstance } from "eosjs";
+import { getAccountName } from "~/lib/eos-helper";
 
 @Component({
-  name: "inspect-page"
+  name: "inspect-page",
+  watch: {
+    network: "loadAccount",
+    identity: "loadAccount"
+  }
 })
 export default class extends Vue {
   @State identity;
   @State network;
   @State networkEndpoint;
   @State chainId;
+
+  _account: any = null;
+
+  get account(): any {
+    if (this._account) {
+      return this._account;
+    } else {
+      return "Loading...";
+    }
+  }
 
   get eos(): EosInstance | null {
     if (this.network && this.networkEndpoint) {
@@ -35,6 +50,22 @@ export default class extends Vue {
     } else {
       return null;
     }
+  }
+
+  get ready() {
+    return this.network && this.eos && this.identity;
+  }
+
+  async loadAccount() {
+    if (!this.ready) {
+      return;
+    }
+
+    const acc = await this.eos!.getAccount({
+      account_name: getAccountName(this.identity)
+    });
+
+    this._account = acc;
   }
 
   contract = {
