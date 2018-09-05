e02b3f9446b120792272dc9b82c6081794c44fca
Aler Denisov <aler.zampillo@gmail.com>
2018-09-05 21:07:16 +0300

    use normalizer and validators

--- components/editors/EditorWrapper.vue
+++ components/editors/EditorWrapper.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   el-card(:class="b()" shadow="hover")
-    div(slot="header") 
-      span {{ name }}
-    component(v-if="base" :is="knownTypes[type].component" v-bind="knownTypes[type].config" @input="onInput")
+    div(slot="header" class="clearfix") 
+      span(:class="b('name')") {{ name }}
+      span(v-if="base" :class="b('type')" style="float: right") {{ type }}
+    component(v-if="base" :class="b('editor')" :is="knownType.component" v-bind="knownType.config" @input="onInput")
     div(v-else-if="nested")
       el-editor-wrapper(v-for="(childType, childName) in type" :type="childType" :name="childName" @input="data => onNested(childName, data)")
     div(v-else-if="array")
@@ -23,9 +24,15 @@ import { TypeDef, knownTypes } from "~/lib/eos-types";
 
 @Component({
   name: "el-editor-wrapper",
-  mounted() {
+  async mounted() {
     if (this.base) {
-      this.$emit("input", knownTypes[this.type].default(this.type, this.name));
+      const def = await this.knownType.default(this.type, this.name);
+      const normalized = await this.knownType.normalize(
+        def,
+        this.type,
+        this.name
+      );
+      this.$emit("input", normalized);
     }
   }
 })
@@ -44,14 +51,19 @@ export default class EditorWrapper extends Vue {
     this.$emit("input", this.value);
   }
 
-  @Emit("input")
-  onInput(newValue) {
+  async onInput(newValue) {
     this.value = newValue;
-    return newValue;
+    const normalized = await this.knownType.normalize(newValue);
+    console.assert(await this.knownType.validation(normalized));
+    this.$emit("input", normalized);
   }
 
-  get knownTypes() {
-    return knownTypes;
+  get knownType() {
+    if (this.base) {
+      return knownTypes[this.type as string];
+    } else {
+      throw new Error("Unknown type");
+    }
   }
 
   get base(): boolean {
